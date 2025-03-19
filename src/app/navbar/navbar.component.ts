479255c38a28c22ee78a70cff65986e09b96c15b
airell <i450046@fhict.nl>
2025-03-19 17:32:11 +0100

    Added navbar component and inserted some placeholder content

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  imports: [CommonModule]
+})
+export class NavbarComponent {
+  isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
+  }
+}

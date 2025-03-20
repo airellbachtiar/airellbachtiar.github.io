0495779709e6c4001ccb91d83ac2299e34764b5e
airell <i450046@fhict.nl>
2025-03-20 18:30:46 +0100

    Added component's section and placeholder content

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'airellbachtiar.github.io' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('airellbachtiar.github.io');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,16 @@ import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from "./profile/profile.component";
+import { AboutComponent } from "./about/about.component";
+import { EducationComponent } from "./education/education.component";
+import { ExperiencesComponent } from "./experiences/experiences.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ContactComponent } from "./contact/contact.component";
 import { SharedService } from './shared.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, ProfileComponent],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent, AboutComponent, EducationComponent, ExperiencesComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
